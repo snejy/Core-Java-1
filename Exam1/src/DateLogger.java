609d6ac5edc091a50621607f0ceb1961edad5aa8
snejy <snspasova@gmail.com>
2014-06-15 17:08:18 +0300

    add class DateLogger

--- Exam1/src/DateLogger.java
+++ Exam1/src/DateLogger.java
@@ -1,4 +1,45 @@
 
-public class DateLogger extends Logger{
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+
+public class DateLogger extends Logger{
+	public Date time;
+	public DateFormat dateFormat = new SimpleDateFormat("|hh:mm:ss dd.MM.YY| ");
+
+	public DateLogger(int level) {
+		super(level);
+	}
+
+	public DateLogger() {
+		super();
+	}
+
+	@Override
+	public void log(String message){
+		if(super.getLevel() >= 3){
+			time = new Date();
+			System.out.println(dateFormat.format(time) + super.getLevel() + " => " + message);
+		}
+	}
+
+	@Override
+	public void log(int level, String message){
+		if(level <= 0){
+			throw new LevelOutOfRangeException();
+		}
+		try{
+			if(level > super.getLevel()){
+				return;
+			}
+			else{
+				time = new Date();
+				System.out.println(dateFormat.format(time) + level + " => " + message);
+			}
+		}
+		catch (LevelOutOfRangeException exception){
+			exception.printStackTrace();
+		}
+	}
 }
--- Exam1/src/Logger.java
+++ Exam1/src/Logger.java
@@ -42,14 +42,5 @@ public class Logger {
 		}
 	}
 	
-	public static void main(String[] args) {
-		Logger logger = new Logger(3);
-		logger.log(2, "Somewhat important message");
-		logger.log(3, "Less important message"); 
-		logger.log(5, "Not important");
-		logger.log("Meh"); 
-		logger.setLevel(2);
-		logger.log("My message");
-	}
 } 
  
